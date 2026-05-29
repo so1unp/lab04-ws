17f119069e8f5c8e8c942c8f2b4cbf2a9d73a0c6
Alexis01-hub <mamanialexis29@gmail.com>
2026-05-29 19:15:57 -0300

    inicio

--- nave.c
+++ nave.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+struct Nave{
+    int oxigeno;
+    int combustible;
+    int posX, posY;
+    int bodegaMinerales[4];
+    pthread_mutex_t mutex;
+} ;
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
+    /*hilos del cliente nave*/
+    void* hilo_soporte_vital(void* arg);
+    void* hilo_propulsion(void* arg);
+    void* hilo_extraccion(void* arg);
+    void* hilo_radar(void* arg);
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
